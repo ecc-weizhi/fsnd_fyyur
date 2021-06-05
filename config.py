784e66e37bbff9632318964aa6a9e2f2d1924116
ecc-weizhi <ecc.weizhi@gmail.com>
2021-06-05 11:32:22 -0700

    Add database URL

--- config.py
+++ config.py
@@ -9,5 +9,4 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://fyuur_user:test1234@localhost:5432/fyuur'
