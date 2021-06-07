b0ccdffdaa4e9f6119310b6a689879d761494542
ecc-weizhi <ecc.weizhi@gmail.com>
2021-06-06 22:26:43 -0700

    Implement create venue

--- app.py
+++ app.py
@@ -197,14 +197,38 @@ def create_venue_form():
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
+    form = VenueForm()
 
-    # on successful db insert, flash success
-    flash('Venue ' + request.form['name'] + ' was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+    has_error = False
+    name = None
+    try:
+        name = form.name.data
+        venue = Venue(
+            name=name,
+            city=form.city.data,
+            state=form.state.data,
+            address=form.address.data,
+            phone=form.phone.data,
+            image_link=form.image_link.data,
+            genres=form.genres.data,
+            facebook_link=form.facebook_link.data,
+            website_link=form.website_link.data,
+            seeking_talent=form.seeking_talent.data,
+            seeking_description=form.seeking_description.data,
+        )
+        db.session.add(venue)
+        db.session.commit()
+        # on successful db insert, flash success
+        flash('Venue ' + venue.name + ' was successfully listed!')
+    except:
+        has_error = True
+        db.session.rollback()
+    finally:
+        db.session.close()
+
+    if has_error:
+        flash(f"An error occurred. Venue{f' {name} ' if name else ' '}could not be listed.")
+
     return render_template('pages/home.html')
 
 
